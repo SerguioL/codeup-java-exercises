64e72f8a874e846016abc346080ae314b2a7e8ec
SerguioL <serguio.l.landa@gmail.com>
2021-09-03 16:26:30 -0500

    src/JavaMini1.java done

---
+++ src/JavaMini1.java
@@ -0,0 +1,18 @@
+public class JavaMini1 {
+    public static void main(String[] args) {
+//        write a short Java program that tests if a number is positive or negative. Define a few numeric variables and run them through a decision. If the number is positive, output that it is positive, and if it is negative, output that it is negative.
+
+// =============================================================
+
+        int input = -1;
+
+        if(input > 0){
+            System.out.printf("The number %s is positive", input);
+        } else {
+            System.out.printf("The number %s is negative", input);
+        }
+
+
+
+    }
+}
