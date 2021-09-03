e0fc048104276cc12605757951984be8207a8541
SerguioL <serguio.l.landa@gmail.com>
2021-09-03 10:59:56 -0500

    finished exercise 1

---
+++ src/ConsoleIOLecture.java
@@ -0,0 +1,9 @@
+public class ConsoleIOLecture {
+    public static void main(String[] args) {
+        String myCohort ="Draco";
+        String timeOfDay = "moring";
+//        System.out.println(myCohort);
+        System.out.printf("Good %s, %s!%n", timeOfDay, myCohort);
+        System.out.printf("Hello, !");
+    }
+}
