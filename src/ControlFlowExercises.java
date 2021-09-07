ea1aa9f73c1068bd2e6b2d70ebd37d68595b3e3b
SerguioL <serguio.l.landa@gmail.com>
2021-09-07 12:11:33 -0500

    done with 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -75,7 +75,15 @@ public class ControlFlowExercises {
         //==========================================================
 
         for(int i = 1;i<100;i++){
-            if
+            if(i % 3 == 0 && i % 5 ==0){
+                System.out.println("FizzBuzz");
+            }else if (i % 3 == 0 ){
+                System.out.println("Fizz");
+            } else if(i % 5 ==0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
 
     }
