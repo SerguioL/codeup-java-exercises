fc0dab5c85883aeb5a0a918616f51fc92dd751cf
SerguioL <serguio.l.landa@gmail.com>
2021-09-07 12:05:34 -0500

    done with 1 and starting 2

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,83 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+        //==========================================================
+
+//        int i = 5;
+//        while(i < 15){
+//            System.out.println("The current number is " + i);
+//            i++;
+//            System.out.println("Now the current number is " + i);
+//        }
+
+        //==========================================================
+
+//        int i = 0;
+//        do{
+//            System.out.println(i);
+//            i= i+2;
+//        }
+//        while (i<100);
+//        System.out.println(i);
+
+        //==========================================================
+
+//        int i = 100;
+//        do{
+//            System.out.println(i);
+//            i= i-5;
+//        }
+//        while (i > -10);
+//        System.out.println(i);
+
+        //==========================================================
+
+//        long i = 2;
+//        do{
+//            System.out.println(i);
+//            i = i * i;
+//        }
+//        while (i <1000000);
+
+        //==========================================================
+
+//        int i;
+//        for (i=5; i<15;i++){
+//            System.out.println("The current number is " + i);
+//        }
+//        System.out.println("The last number is "+ i);
+
+        //==========================================================
+
+//        int i;
+//        for(i=0; i<100; i+=2){
+//            System.out.println(i);
+//        }
+//        System.out.println(i);
+//
+        //==========================================================
+
+//        int i;
+//        for(i =100; i> -10; i-=5){
+//            System.out.println(i);
+//        }
+//        System.out.println(i);
+
+        //==========================================================
+
+//        long i;
+//        for(i=2; i<1000000; i*=i){
+//            System.out.println(i);
+//
+//        }
+
+        //==========================================================
+
+        for(int i = 1;i<100;i++){
+            if
+        }
+
+    }
+
+}
