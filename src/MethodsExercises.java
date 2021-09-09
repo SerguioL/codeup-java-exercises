aaa90ce80131d89990583b1a8668894581f574c3
SerguioL <serguio.l.landa@gmail.com>
2021-09-08 20:38:54 -0500

    finished exercses 1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,6 +3,8 @@ public class MethodsExercises {
     public static void main(String[] args) {
         System.out.println(addition(2,8));
         System.out.println(subtraction(8,2));
+        System.out.println(multiplication(2,8));
+        System.out.println(division(8,2));
     }
 
      public static int addition(int input1, int input2){
@@ -11,6 +13,14 @@ public class MethodsExercises {
 
     public static int subtraction(int input1, int input2){
         return input1 - input2;
+    }
+
+    public static int multiplication(int input1, int input2){
+    return input1 * input2;
+    }
+
+    public static int division(int input1, int input2){
+        return input1/input2;
     }
 
 }
