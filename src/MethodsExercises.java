6a097b6b615627380783eb4631aa04bb21d42f54
SerguioL <serguio.l.landa@gmail.com>
2021-09-08 20:33:37 -0500

    add both addition and subtraction methods/functions

---
+++ src/MethodsExercises.java
@@ -0,0 +1,16 @@
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+        System.out.println(addition(2,8));
+        System.out.println(subtraction(8,2));
+    }
+
+     public static int addition(int input1, int input2){
+        return input1 + input2;
+     }
+
+    public static int subtraction(int input1, int input2){
+        return input1 - input2;
+    }
+
+}
