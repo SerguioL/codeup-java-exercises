a326013803ab12a1815022973fc29f6f38a513ea
SerguioL <serguio.l.landa@gmail.com>
2021-09-10 13:50:51 -0500

    converted method

---
+++ src/AnimalTest.java
@@ -0,0 +1,12 @@
+public class AnimalTest {
+
+    public static void main(String[] args) {
+        Animal luna = new Animal("Luna","dog","Bark");
+//        Luna.name= "Luna";
+//        Luna.animal= "dog";
+//        Luna.sound= "Bark";
+//        Luna.makeNoise();
+        Animal garfield = new Animal("Garfield","cat","meow");
+    }
+
+}
