a6532d3a2968210b101ab1738c545be2e9567c9d
SerguioL <serguio.l.landa@gmail.com>
2021-09-10 16:07:37 -0500

    almost done

---
+++ src/InputTest.java
@@ -0,0 +1,31 @@
+
+import util.Input;
+public class InputTest {
+
+    public static void main(String[] args) {
+//        Input input = new Input();
+//        String userInput = input.getSting();
+//        System.out.println(userInput);
+
+        //=====================================
+
+//        Input input2 = new Input();
+//        Boolean userInput2 = input2.yesNo();
+//        System.out.println(userInput2);
+
+        //=====================================
+
+//        Input input3 = new Input();
+//        int userInput3 = input3.getInt(1,10);
+//        System.out.println(userInput3);
+
+        //=====================================
+
+        Input input4 = new Input();
+        double userInput4 = input4.getDouble(1,10);
+        System.out.println(userInput4);
+
+
+    }
+
+}
