1111ba5aa1d69b7d4db0715f9891acac68e5f911
SerguioL <serguio.l.landa@gmail.com>
2021-09-14 10:23:42 -0500

    finished just forgot to push

--- src/InputTest.java
+++ src/InputTest.java
@@ -2,15 +2,19 @@ import util.Input;
 public class InputTest {
 
     public static void main(String[] args) {
-//        Input input = new Input();
-//        String userInput = input.getSting();
-//        System.out.println(userInput);
+        Input input = new Input();
+        String userInput = input.getSting();
+        System.out.println(userInput);
 
         //=====================================
 
 //        Input input2 = new Input();
 //        Boolean userInput2 = input2.yesNo();
 //        System.out.println(userInput2);
+
+        //testing
+//        boolean yesNoResult = input.yesNo(userInput);
+//        System.out.println(yesNoResult? "User said yes!": "User said no");
 
         //=====================================
 
@@ -20,9 +24,9 @@ public class InputTest {
 
         //=====================================
 
-        Input input4 = new Input();
-        double userInput4 = input4.getDouble(1,10);
-        System.out.println(userInput4);
+//        Input input4 = new Input();
+//        double userInput4 = input4.getDouble(1,10);
+//        System.out.println(userInput4);
 
 
     }
