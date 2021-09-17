c3aa25cc9989204017048db88e0ff9debe5ea67a
SerguioL <serguio.l.landa@gmail.com>
2021-09-17 14:24:25 -0500

    finished

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,14 +1,27 @@
 package shapes;
-
+//
 public class ShapesTest {
     public static void main(String[] args) {
-// variable type name       instance
-        Rectangle box1 = new Rectangle(4,5);
-        System.out.println(box1.getPerimeter() + " " + box1.getArea());
+//// variable type name       instance
+//        Rectangle box1 = new Rectangle(4,5);
+//        System.out.println(box1.getPerimeter());
+//        System.out.println(box1.getArea());
+//
+//        //this is Polymorphism
+//// variable type name       instance
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getPerimeter());
+//        System.out.println(box2.getArea());
+//
+//
 
-// variable type name       instance
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getArea() + " " + box2.getPerimeter());
+        Measurable myShape1 = new Rectangle(4,5);
+        System.out.println(myShape1.getArea());
+        System.out.println(myShape1.getPerimeter());
+//        System.out.println(myShape1.ge);
+        Measurable myShape2 = new Square(5);
+        System.out.println(myShape2.getArea());
+        System.out.println(myShape2.getPerimeter());
 
 
     }
