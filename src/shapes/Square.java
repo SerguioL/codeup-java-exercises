56a8cc2d6a1c24f331d2f0303b8acebae47f5f4e
SerguioL <serguio.l.landa@gmail.com>
2021-09-17 14:27:12 -0500

    finished

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,23 +1,52 @@
 package shapes;
 
-public class Square extends Rectangle{
-    //properties
-    protected double side;
+public class Square extends Quadrilateral{
 
-    // constructor
-    // double holds decimals
-    public Square(double side){//argument
-        super(side,side);//super = parent \super is referring to the Rectangle constructor because Square is a sub-class of Rectangle
-        this.side = side;
-        System.out.println("This is for a Square");
+    public Square(double side) {
+        super(side,side);
+        this.side = length;
+        this.side = width;
     }
 
-    public double getArea(){
+    @Override
+    public double getPerimeter() {
+        return 4 * this.side;
+    }
+
+    @Override
+    public double getArea() {
         return Math.pow(this.side , 2);
     }
 
-    public double getPerimeter(){
-        return 4 * this.side;
+    @Override
+    public void setLength(double length) {
+        this.length =length;
+        this.width = length;
     }
+
+    @Override
+    public void setWidth(double width) {
+        this.length = width;
+        this.width = width;
+    }
+    //properties
+    protected double side;
+//
+//    // constructor
+//    // double holds decimals
+//    public Square(double side){//argument
+//        super(side,side);//super = parent \super is referring to the Rectangle constructor because Square is a sub-class of Rectangle
+//        this.side = side;
+//    }
+//
+//    public double getArea(){
+//        System.out.println("This is for a Square");
+//        return Math.pow(this.side , 2);
+//    }
+//
+//    public double getPerimeter(){
+//        System.out.println("This is for a Square");
+//        return 4 * this.side;
+//    }
 
 }
