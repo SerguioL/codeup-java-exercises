ed133449dea1e942d672a5c779bb4801fe4b0e2a
SerguioL <serguio.l.landa@gmail.com>
2021-10-14 09:27:04 -0500

    still working on it

---
+++ src/JavaDrills.java
@@ -0,0 +1,22 @@
+public class JavaDrills {
+//working progress
+    public static String flipOuterCase(String input){
+
+        for(int i= 0;i<input.length();i++){
+            if()
+        }
+//        String fistLetter = input.substring(0,1).toUpperCase();
+//        String lastLetter = input.substring(0,input.length()-1).toUpperCase();
+//        return lastLetter;
+
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(flipOuterCase('cat')); // CaT
+//        System.out.println(flipOuterCase('CaT')); // cat
+//        System.out.println(flipOuterCase('caT')); // Cat
+//        System.out.println(flipOuterCase('cAt')); // CAT
+        System.out.println(flipOuterCase("cat"));
+    }
+
+}
