af1d0b6aaeb39a5444df711754157bd2ead21961
SerguioL <serguio.l.landa@gmail.com>
2022-02-10 21:20:27 -0600

    codewars

---
+++ src/codewars/WhoLikesIt.java
@@ -0,0 +1,42 @@
+package codewars;
+
+
+public class WhoLikesIt {
+
+//    You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+//
+//    Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+//
+//            []                                -->  "no one likes this"
+//            ["Peter"]                         -->  "Peter likes this"
+//            ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+//            ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+//            ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+    public static String whoLikesIt(String... names) {
+
+//        System.out.println(names);
+
+        if(names.length == 0){
+            return "no one likes this";
+        } else if (names.length == 1){
+            return names[0] + " likes this";
+        } else if (names.length == 2){
+            return names[0] + " and " + names[1] + " like this";
+        } else if (names.length == 3){
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        } else {
+            return names[0] + ", " + names[1] + ", and " + (names.length - 2 )+ " others like this";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(whoLikesIt());//"no one likes this"
+        System.out.println(whoLikesIt("Peter"));//"Peter likes this"
+        System.out.println(whoLikesIt("Jacob", "Alex"));//"Jacob and Alex like this"
+        System.out.println(whoLikesIt("Max", "John", "Mark"));//"Max, John and Mark like this"
+        System.out.println(whoLikesIt("Alex", "Jacob", "Mark", "Max"));//"Alex, Jacob and 2 others like this"
+    }
+
+}
